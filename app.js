fc1301520957793abae3b5fd3f21e32885371716
jkayk <jirehkang10@gmail.com>
2020-09-02 10:07:57 +0900

    added more modules

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(logger("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
