d4511e509f62518f580a9b62c61c5a3ecf8a525e
jkayk <jirehkang10@gmail.com>
2020-09-02 23:41:40 +0900

    began using pug

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ const CHANGE_PASSWORD = "/change-password";
 //videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
-const VIDEO_DETAIL = "/:id"; //: shows that it will change to the "id"
+const VIDEO_DETAIL = "/:id"; //: shows that it will change to whatever the "id" is
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
