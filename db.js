94b20d96cf8e9f68156fbbfe7747b5515648cdbe
jkayk <jirehkang10@gmail.com>
2020-09-04 12:26:39 +0900

    added middleware, updated more htmls

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 234235236,
+    title: "Awesome Video",
+    description: "this is an awesome video",
+    views: 465,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 15642,
+      name: "Kay",
+      email: "k@k.com",
+    },
+  },
+  {
+    id: 56421,
+    title: "Excellent Video",
+    description: "this is an Excellent video",
+    views: 465,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 15642,
+      name: "Kay",
+      email: "k@k.com",
+    },
+  },
+  {
+    id: 12319,
+    title: "Incredible Video",
+    description: "this is an Incredible video",
+    views: 465,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 15642,
+      name: "Kay",
+      email: "k@k.com",
+    },
+  },
+  {
+    id: 4589456,
+    title: "Ultimate Video",
+    description: "this is an Ultimate video",
+    views: 465,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 15642,
+      name: "Kay",
+      email: "k@k.com",
+    },
+  },
+];
